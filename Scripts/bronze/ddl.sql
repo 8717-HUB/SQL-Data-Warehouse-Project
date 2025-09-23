34a56593c7bc23d017043e9f62c0fd5e833346e2
KEVIN JUNIOR <junioranalyst2021@gmail.com>
2025-09-23 16:03:36 +0300

    Create ddl.sql

---
+++ Scripts/bronze/ddl.sql
@@ -0,0 +1,81 @@
+CREATE OR ALTER PROCEDURE bronze.load_bronze AS 
+BEGIN 
+  DECLARE @start_time DATETIME, @end_time DATETIME
+  SET @start_time = GETDATE()
+  BEGIN TRY
+    PRINT'==============================================================';
+	PRINT'----------------------Loading CRM Tables----------------------';
+	PRINT'==============================================================';
+    TRUNCATE TABLE Bronze.crm_cust_info; ---- OVERWRITE THE TABLE FIRST BEFORE INSERTING DATA
+	PRINT'>>>Inserting table:Bronze.crm_cust_info<<<';
+	BULK INSERT Bronze.crm_cust_info
+	FROM 'C:\Users\HomePC\Desktop\data science\DATA_WAREHOUSE_PROJECT\sql-data-warehouse-project\datasets\source_crm\cust_info.csv'
+	WITH (
+		   FIRSTROW = 2,
+		   FIELDTERMINATOR= ',',
+		   TABLOCK
+		);
+	TRUNCATE TABLE bronze.crm_prd_info
+	PRINT'>>>Inserting table:bronze.crm_prd_info<<<';
+	BULK INSERT bronze.crm_prd_info
+	FROM 'C:\Users\HomePC\Desktop\data science\DATA_WAREHOUSE_PROJECT\sql-data-warehouse-project\datasets\source_crm\prd_info.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+		);
+	TRUNCATE TABLE bronze.crm_sales_details
+	PRINT'>>>Inserting table:bronze.crm_sales_details<<<';
+	BULK INSERT bronze.crm_sales_details
+	FROM 'C:\Users\HomePC\Desktop\data science\DATA_WAREHOUSE_PROJECT\sql-data-warehouse-project\datasets\source_crm\sales_details.csv'
+	WITH (
+		FIRSTROW = 2,
+		FIELDTERMINATOR = ',',
+		TABLOCK
+		);
+	PRINT'==============================================================';
+	PRINT'----------------------Loading ERP Tables----------------------';
+	PRINT'==============================================================';
+
+    PRINT'>>>Truncating table:bronze.erp_cust_az12';
+	PRINT'==============================================================';
+	TRUNCATE TABLE bronze.erp_cust_az12
+	PRINT'>>>Inserting table:bronze.erp_cust_az12';
+	BULK INSERT bronze.erp_cust_az12
+	FROM 'C:\Users\HomePC\Desktop\data science\DATA_WAREHOUSE_PROJECT\sql-data-warehouse-project\datasets\source_erp\CUST_AZ12.csv'
+	WITH (
+		 FIRSTROW = 2,
+		 FIELDTERMINATOR= ',',
+		 TABLOCK
+	);
+	TRUNCATE TABLE bronze.erp_loc_a101
+	PRINT'>>>Inserting table:bronze.erp_loc_a101<<<'
+	BULK INSERT bronze.erp_loc_a101
+	FROM 'C:\Users\HomePC\Desktop\data science\DATA_WAREHOUSE_PROJECT\sql-data-warehouse-project\datasets\source_erp\LOC_A101.csv'
+	WITH (
+	   FIRSTROW= 2,
+	   FIELDTERMINATOR= ',',
+	   TABLOCK
+	   );
+	TRUNCATE TABLE bronze.erp_px_cat_g1v2
+	PRINT'>>>Inserting table:bronze.erp_px_cat_g1v2<<<'
+	BULK INSERT bronze.erp_px_cat_g1v2
+	FROM 'C:\Users\HomePC\Desktop\data science\DATA_WAREHOUSE_PROJECT\sql-data-warehouse-project\datasets\source_erp\PX_CAT_G1V2.csv'
+	WITH (
+		FIRSTROW= 2,
+		FIELDTERMINATOR= ',',
+		TABLOCK
+		);
+ SET @end_time = GETDATE()
+ PRINT'>>>Total duration'+ CAST (DATEDIFF(second,@start_time,@end_time) AS NVARCHAR) + 'seconds';
+ END TRY
+ BEGIN CATCH
+ PRINT'========================================================';
+ PRINT'------------ERROR OCCURED WHILE LOADING DATA------------';
+ PRINT'ERROR MESSAGE' + ERROR_MESSAGE() + '--------------------';
+ PRINT'========================================================';
+ END CATCH
+END
+
+
+
